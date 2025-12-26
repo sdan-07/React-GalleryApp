665b375ddddb5f9867e0ddb4b3ef2bb5034c368c
sdan-07 <soumyadipdan7@gmai.com>
2025-12-26 17:56:26 +0530

    added page number

--- src/App.jsx
+++ src/App.jsx
@@ -27,26 +27,27 @@ const App = () => {
       <h1 className="text-center lobster-two-bold-italic text-5xl!">Galleria</h1>
       <Image userdata={userData} />
 
-      <div className="mt-15 flex justify-center gap-18">
+      <div className="mt-15 flex justify-center gap-8">
         <button
           onClick={() => {
-            if(Page <= 1) return;
+            if(Page === 1) return;
             setPage((p) => p - 1);
             console.log(Page);
             
           }}
-          className="bg-amber-400 active:scale-95 px-4  sm:px-10 py-4 text-black text-lg font-bold rounded-lg cursor-pointer"
-        >
+          className={`${Page === 1 ? "cursor-not-allowed active:scale-none" : "cursor-pointer active:scale-95"} bg-amber-400 px-4  md:px-10 py-4 text-black text-lg font-bold rounded-lg`}
+        > 
           <CircleArrowLeft size={38} color="black" className="hidden sm:block"/>
-          <CircleArrowLeft size={28} color="black" className="block sm:hidden"/>
+          <CircleArrowLeft size={28} color="black" className="block sm:hidden"/> 
         </button>
+        <h1 className="font-semibold! text-xl! md:text-[25px]! justify-center flex items-center">Page {Page}</h1>
         <button
           onClick={() => {
             setPage((p) => p + 1);
             console.log(Page);
             
           }}
-          className="bg-amber-400 active:scale-95 px-4  sm:px-10 py-4 text-black text-lg font-bold rounded-lg cursor-pointer"
+          className="bg-amber-400 active:scale-95 px-4  md:px-10 py-4 text-black text-lg font-bold rounded-lg cursor-pointer"
         >
           <CircleArrowRight size={38} color="black" className="hidden sm:block"/>
           <CircleArrowRight size={28} color="black" className="block sm:hidden"/>
